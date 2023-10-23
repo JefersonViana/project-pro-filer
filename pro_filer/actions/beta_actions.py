45c818bddb3e79aebdf6c6806c7d5e262189513d
jeferson-viana <jefersonv28@gmail.com>
2023-10-23 15:37:27 -0300

    Requisito 1 concluído

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,8 +5,13 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
-        print(f"Deepest file: {deepest_file}")
+        index = 0
+        length = 0
+        for i, item in enumerate(context["all_files"]):
+            if len(item.split("/")) > length:
+                length = len(item.split("/"))
+                index = i
+        print(f"Deepest file: {context['all_files'][index]}")
 
 
 def find_file_by_name(context, search_term, case_sensitive=True):
