595fc572c3e8d31c67f18eefcc0a16e3ea94344c
jeferson-viana <jefersonv28@gmail.com>
2023-10-24 18:35:49 -0300

    Refatorando requisito 4

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,60 +1,11 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
 from datetime import datetime
-# import pytest
-
-# BASE_URL = ("/home/jeferson-viana/TRYBE/projetos/"
-#             "python-029-python-projeto-pro-filer")
-# DIRECTORY = "/.trybe"
-# FILE = "/requirements.json"
-
-
-# @pytest.mark.parametrize(
-#         "context, result",
-#         [
-#             (
-#                 {
-#                     "base_path": BASE_URL + DIRECTORY,
-#                 },
-#                 ("File name: .trybe\n"
-#                  "File size in bytes: 4096\n"
-#                  "File type: directory\n"
-#                  "File extension: [no extension]\n"
-#                  "Last modified date: 2023-10-23\n")
-#             ),
-#             (
-#                 {
-#                     "base_path": BASE_URL + DIRECTORY + FILE,
-#                 },
-#                 ("File name: requirements.json\n"
-#                  "File size in bytes: 1415\n"
-#                  "File type: file\n"
-#                  "File extension: .json\n"
-#                  "Last modified date: 2023-10-23\n")
-#             ),
-#             (
-#                 {
-#                     "base_path": ("/home/TRYBE/"
-#                                   "python-029-python-projeto-pro-filer"),
-#                 },
-#                 ("File 'python-029-python-projeto-pro-filer'
-# does not exist\n")
-#             )
-#         ]
-# )
-# def test_show_details(capsys, context, result):
-#     show_details(context)
-#     captured = capsys.readouterr()
-#     assert captured.out == result
 
 
 def test_show_details_with_directory(capsys, tmp_path):
     directory = tmp_path / "projetos"
     directory.mkdir()
-    date = directory.stat().st_mtime
-    data_modificacao = datetime.fromtimestamp(date)
-    day = data_modificacao.date().day
-    month = data_modificacao.date().month
-    year = data_modificacao.date().year
+    date = datetime.fromtimestamp(directory.stat().st_mtime).date()
     size = directory.stat().st_size
     context = {
         "base_path": directory.as_posix()
@@ -66,22 +17,25 @@ def test_show_details_with_directory(capsys, tmp_path):
                             "File type: directory\n"
                             "File extension: [no extension]\n"
                             "Last modified date:"
-                            f" {year}-{month}-{day}\n")
+                            f" {date.year}-{date.month}-{date.day}\n")
 
 
 def test_show_details_with_file(capsys, tmp_path):
     file = tmp_path / "requirements.json"
     file.touch()
+    date = datetime.fromtimestamp(file.stat().st_mtime).date()
+    size = file.stat().st_size
     context = {
         "base_path": file.as_posix()
     }
     show_details(context)
     captured = capsys.readouterr()
     assert captured.out == ("File name: requirements.json\n"
-                            "File size in bytes: 0\n"
+                            f"File size in bytes: {size}\n"
                             "File type: file\n"
                             "File extension: .json\n"
-                            "Last modified date: 2023-10-24\n")
+                            "Last modified date: "
+                            f"{date.year}-{date.month}-{date.day}\n")
 
 
 def test_show_details_with_file_not_exist(capsys):
