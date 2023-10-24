0b183c490621d03517a481b7a66ce8986698c7b9
jeferson-viana <jefersonv28@gmail.com>
2023-10-23 21:41:05 -0300

    Requisito 3 concluído

--- pro_filer/actions/main_actions.py
+++ pro_filer/actions/main_actions.py
@@ -74,3 +74,9 @@ def show_preview(context):
     if context["all_files"] or context["all_dirs"]:
         print(f'First 5 files: {context["all_files"][:5]}')
         print(f'First 5 directories: {context["all_dirs"][:5]}')
+
+
+# context = {
+#       "all_dirs": ["src", "src/utils"]
+#     }
+# show_preview(context)
--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,52 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+import pytest
+
+
+@pytest.mark.parametrize(
+        ["context", "result"],
+        [
+            (
+                {
+                    "all_files": ["src/__init__.py",
+                                  "src/app.py", "src/utils/__init__.py"],
+                    "all_dirs": ["src", "src/utils"]
+                },
+                (
+                    "Found 3 files and 2 directories\n"
+                    "First 5 files: ['src/__init__.py', 'src/app.py', "
+                    "'src/utils/__init__.py']\n"
+                    "First 5 directories: ['src', 'src/utils']\n"
+                )
+            ),
+            (
+                {
+                    "all_files": [],
+                    "all_dirs": []
+                },
+                ("Found 0 files and 0 directories\n")
+            ),
+            (
+                {"all_files": [
+                    "src/__init__.py",
+                    "src/app.py",
+                    "src/utils/__init__.py",
+                    "src/todo_list.py",
+                    "src/users.py",
+                    "src/login.py"
+                ],
+                    "all_dirs": ["src", "src/utils"]},
+                (
+                    "Found 6 files and 2 directories\n"
+                    "First 5 files: ['src/__init__.py', 'src/app.py', "
+                    "'src/utils/__init__.py', "
+                    "'src/todo_list.py', "
+                    "'src/users.py']\n"
+                    "First 5 directories: ['src', 'src/utils']\n"
+                )
+            )
+        ]
+)
+def test_show_preview(capsys, context, result):
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == result
