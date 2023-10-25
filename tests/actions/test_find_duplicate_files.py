9a97ca35e664e51d382dd0e696daa8f6e65bc2c6
jeferson-viana <jefersonv28@gmail.com>
2023-10-24 22:05:56 -0300

    Requisito 6 concluído

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -1,1 +1,38 @@
 from pro_filer.actions.main_actions import find_duplicate_files  # NOQA
+import pytest
+
+
+@pytest.mark.parametrize(
+        "context, result",
+        [
+            (
+                {
+                    "all_files": [
+                        "pro_filer/cli.py",
+                        "pro_filer/cli.py"
+                    ]
+                },
+                [("pro_filer/cli.py", "pro_filer/cli.py")]
+            ),
+            (
+                {
+                    "all_files": [
+                        "pro_filer/cli.py",
+                        "pro_filer/cli_helpers.py"
+                    ]
+                },
+                []
+            )
+        ]
+)
+def test_find_duplicate_files(context, result):
+    response = find_duplicate_files(context)
+    assert response == result
+
+
+def test_find_duplicate_files_if_launch_error():
+    context = {
+        "all_files": ["pro_filer/cli.py", "pro_filer/cli_hexlpers.py"]
+    }
+    with pytest.raises(ValueError, match="All files must exist"):
+        find_duplicate_files(context)
